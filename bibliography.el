a914a60b7fb6091333d7ace36591ead6991198da
Anirudh C <anirudh.c@iiitb.org>
2022-09-08 01:44:33 +0530

    add: v0.2 and update documentation

--- bibliography.el
+++ bibliography.el
@@ -5,7 +5,9 @@
 ;; Author: Anirudh Chandramouli <anirudhvan@gmail.com>
 ;; Homepage: https://github.com/Anirudh-C/bibliography-mode
 ;; Keywords: convenience
-;; Version: 0.1
+;; Version: 0.2
+
+;; Package-Requires: ((emacs "27.1") ("org-inline-pdf") ("nano-emacs"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -22,9 +24,20 @@
 ;; For a full copy of the GNU General Public License
 ;; see <https://www.gnu.org/licenses/>.
 
+;;; News
+
+;; Version 0.2
+;; - Added previews and utilities
+;; - Documentation added
+;;
+;; Version 0.1
+;; - Capability to add entries
+
 ;;; Code:
 
 ;; Emacs Core
+(require 'org-indent)
+(require 'org-element)
 (require 'ol-bibtex)
 
 ;; Create a customisation group
@@ -49,9 +62,11 @@
             ""
             (cdr (assq :title entry))))))
 
-(defun add-bib-entries ()
+(defun bibliography/add-bib-entries ()
   "Interactively create new entries with bibtex input"
+
   (interactive)
+  (setq buffer-read-only nil)
   (let ((input
          (let ((keymap (copy-keymap minibuffer-local-map)))
            (define-key keymap (kbd "RET") 'newline)
@@ -64,7 +79,120 @@
       (dotimes (_ entries)
         (save-excursion (org-bibtex-write))
         (re-search-forward org-property-end-re)
-        (open-line 1) (forward-char 1)))))
+        (open-line 1) (forward-char 1))))
+  (save-buffer)
+  (setq buffer-read-only t))
+
+(defun bibliography/tag-entry ()
+  "Interactively tag entry at current point"
+
+  (interactive)
+  (setq buffer-read-only nil)
+  (org-set-tags-command)
+  (save-buffer)
+  (setq buffer-read-only t))
+
+(defun preview ()
+  "Build a preview of the entry at point."
+
+  (let* ((author (org-entry-get (point) "AUTHOR"))
+         (title (org-entry-get (point) "ITEM"))
+         (doi (org-entry-get (point) "DOI"))
+         (year (org-entry-get (point) "YEAR"))
+         (pdf (org-entry-get (point) "PDF"))
+         (filename (buffer-file-name (current-buffer)))
+         (custom-id (org-entry-get (point) "CUSTOM_ID"))
+         (journal (or (org-entry-get (point) "JOURNAL")
+                      (org-entry-get (point) "CONFERENCE")
+                      (if (string= (org-entry-get (point) "BTYPE") "book") "BOOK")
+                      (if (member "PREPRINT" (org-get-tags)) "PREPRINT")
+                      "ONLINE"))
+         (notes (save-excursion
+                  (org-goto-first-child)
+                  (org-goto-sibling)
+                  (let* ((element (org-element-at-point))
+                         (beg (org-element-property :robust-begin element))
+                         (end (org-element-property :robust-end element)))
+                    (if (and beg end)
+                        (buffer-substring-no-properties beg (+ end 1))))))
+         (notes (if notes
+                    (string-trim notes)
+                  "None.")))
+
+    (concat
+     (propertize (format "[[file:%s::#%s][%s (%s)]]\n"
+                         filename  custom-id (upcase journal) year)
+                 'face '(:inherit (nano-popout nano-strong) :height 0.85))
+     (propertize (format "*%s*\n" title))
+     (propertize (format "/%s/\n\n" author) 'face '(:inherit default :height 0.85))
+     (concat
+      (propertize "*Notes.* " 'face 'bold)
+      (if notes
+          (propertize notes 'face '(:inherit nano-default))
+        "")
+      "\n\n")
+     (propertize (format "[[%s]]" pdf)))))
+
+(defun hide-preview ()
+  "Delete preview window"
+
+  (interactive)
+  (delete-other-windows)
+  (when (get-buffer "*bib-preview*")
+    (kill-buffer "*bib-preview*")))
+
+(defun show-pdf ()
+  "Show PDF in dedicated buffer"
+
+  (interactive)
+  (hide-preview)
+  (when (org-entry-get (point) "PDF")
+    (find-file (org-entry-get (point) "PDF"))
+    (setq-local header-line-format nil)))
+
+(defun show-preview ()
+  "View preview of current item in a dedicated preview buffer."
+
+  (interactive)
+  (hide-preview)
+  (let ((preview))
+    (save-excursion
+      (if (and (eq 1 (org-element-property :level (org-element-at-point)))
+               (org-goto-first-child))
+          (let ((sibling t))
+            (while sibling
+              (setq preview (concat preview (preview)))
+              (setq sibling (org-goto-sibling))))
+        (setq preview (preview))))
+
+    (split-window-right)
+    (let ((buf (current-buffer)))
+      (switch-to-buffer-other-window (get-buffer-create "*bib-preview*"))
+      (let ((inhibit-read-only t))
+        (delete-region (point-min) (point-max))
+        (insert preview)
+        (visual-line-mode)
+        (hl-line-mode)
+        (org-mode)
+        (setq-local cursor-type nil)
+        (setq org-image-actual-width (list (window-width nil t)))
+        (setq-local org-return-follows-link t)
+        (setq buffer-read-only t)
+        (setq header-line-format nil)
+        (goto-char (point-min))
+        (switch-to-buffer-other-window buf)))))
+
+(defun import-bib-entries (file)
+  "Import bib entries from file"
+
+  (interactive "fFile: ")
+  (setq buffer-read-only nil)
+  (dotimes (_ (org-bibtex-read-file file))
+    (save-excursion (org-bibtex-write))
+    (re-search-forward org-property-end-re)
+    (open-line 1) (forward-char 1))
+  (save-buffer)
+  (setq buffer-read-only t))
 
 (defun bibliography-activate ()
   "Activates bibliography mode."
@@ -72,15 +200,24 @@
   ;; Set org-bibtex headline format
   (setq org-bibtex-headline-format-function #'bibliography-headline-format)
 
-  (with-current-buffer (current-buffer)
-    (let ((map (current-local-map)))
-      (define-key map (kbd "C-c b a") 'add-bib-entry))))
+  ;; Set read-write permissions
+  (setq buffer-read-only t))
 
 (defun bibliography-deactivate ()
   "De-activates bibliography mode"
 
+  (interactive)
   ;; Reset default org-bibtex headline format
-  (setq org-bibtex-headline-format-function #'(lambda (entry) (cdr (assq :title entry)))))
+  (setq org-bibtex-headline-format-function #'(lambda (entry) (cdr (assq :title entry))))
+
+  ;; Restore read-write permissions
+  (setq buffer-read-only nil)
+
+  ;; Hide any open previews
+  (hide-preview)
+
+  ;; Kill the bibliography buffer
+  (kill-buffer (current-buffer)))
 
 ;; The minor mode
 (define-minor-mode bibliography-mode
@@ -89,6 +226,17 @@
   :initial-value t
   :global nil
   :lighter "bibliography"
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "a") 'add-bib-entries)
+            (define-key map (kbd "i") 'import-bib-entries)
+            (define-key map (kbd "e") 'org-bibtex)
+            (define-key map (kbd "t") 'tag-entry)
+            (define-key map (kbd "SPC") 'show-preview)
+            (define-key map (kbd "RET") 'hide-preview)
+            (define-key map (kbd "o") 'show-pdf)
+            (define-key map (kbd "s") 'org-tags-view)
+            (define-key map (kbd "q") 'bibliography-deactivate)
+            map)
 
   (if bibliography-mode
       (bibliography-activate)
