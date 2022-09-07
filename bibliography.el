87f35d79f9a8e5322b3b053b267d37534c81cc53
Anirudh C <anirudh.c@iiitb.org>
2022-09-07 21:55:50 +0530

    add: package header with version 0.1

--- bibliography.el
+++ bibliography.el
@@ -1,3 +1,29 @@
+;;; bibliography.el --- Annotated Bibliography minor-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 Anirudh Chandramouli
+
+;; Author: Anirudh Chandramouli <anirudhvan@gmail.com>
+;; Homepage: https://github.com/Anirudh-C/bibliography-mode
+;; Keywords: convenience
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
 ;; Emacs Core
 (require 'ol-bibtex)
 
